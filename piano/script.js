7d1f95bfd297ae23020332cd17ea6e9fd68d2ad3
B-sirius <padfoot_07@163.com>
2017-06-21 08:16:17 +0800

    audio test

--- piano/script.js
+++ piano/script.js
@@ -21,17 +21,27 @@ let audioCtx = new AudioContext();
     }
 
     Key.prototype.makeSound = function() {
-
+        // 创建一个声音（周期性波形）
         let oscillator = audioCtx.createOscillator();
+        // 创建一个音量大小
         let gainNode = audioCtx.createGain();
+        // 关联声音和音量
         oscillator.connect(gainNode);
+        // 关联声音与音频输出设备
         gainNode.connect(audioCtx.destination);
-        oscillator.type = 'sine';
+        // 声音波形，有正弦sine, 方波square, 三角波triangle, 锯齿波sawtooth
+        oscillator.type = 'sine'; // 正弦波
+        // 波形的频率，数值越大越清脆
         oscillator.frequency.value = this.frequency;
+        // 设置当下音量为0
         gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+        // 在0.01秒内将声音线性从0变到1
         gainNode.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 0.01);
+        // 播放这个声音
         oscillator.start(audioCtx.currentTime);
+        // 1秒内将声音从1指数曲线变为0.001
         gainNode.gain.exponentialRampToValueAtTime(0.001, audioCtx.currentTime + 1);
+        // 一秒后，声音关闭
         oscillator.stop(audioCtx.currentTime + 1);
     }
 
