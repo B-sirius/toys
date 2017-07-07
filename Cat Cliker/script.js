f3e29368c2f21d87e07ae8ac0e4dfd18618859dc
B-sirius <padfoot_07@163.com>
2017-07-07 16:56:29 +0800

    cat clicker personal version

--- Cat Cliker/script.js
+++ Cat Cliker/script.js
@@ -1,17 +1,90 @@
 'use strict';
 
-let catPicture = document.getElementById('picture');
-let countEl = document.getElementById('count');
+let Clicker = function(imgURL, title) {
+    this.imgURL = imgURL;
+    this.title = title;
+    this.count = 0;
 
-let renderCount = (function() {
-    let count = 0;
+}
 
-    catPicture.addEventListener('click', () => {
-        count++;
-        renderCount();
+// 生成一个clicker元素
+Clicker.prototype.init = function() {
+    let container = document.createElement('div');
+
+    let img = new Image();
+    img.className = 'img';
+    img.src = this.imgURL;
+
+    let title = document.createElement('h2');
+    title.className = 'title';
+    title.textContent = this.title;
+
+    let count = document.createElement('span');
+    count.className = 'count';
+    count.textContent = this.count;
+
+    container.appendChild(title);
+    container.appendChild(img);
+    container.appendChild(count);
+
+    this._initCount(img, count);
+
+    return container;
+}
+
+Clicker.prototype._initCount = function(clickEl, count) {
+    let self = this;
+    clickEl.addEventListener('click', function() {
+        self.count++;
+
+        count.textContent = self.count;
+    })
+}
+
+let clickerData = [{
+    imgURL: './img/cat1.jpg',
+    title: '黑老大'
+}, {
+    imgURL: './img/cat2.jpg',
+    title: '蓝珠子'
+}]
+
+let clickerCache = {};
+
+let initList = function() {
+    let fragment = document.createDocumentFragment();
+    let clickerList = document.getElementById('clickerList');
+    let clickerContainer = document.getElementById('clickerContainer');
+
+    clickerData.forEach((data, index) => {
+        let { title, imgURL} = data;
+
+        let clickerItem = document.createElement('li');
+        clickerItem.className = 'clicker-item';
+
+        let btn = document.createElement('a');
+        btn.className = 'btn';
+        btn.textContent = title;
+
+        btn.addEventListener('click', () => {
+            let clicker;
+            if (clickerCache[index]) {
+                clicker = clickerCache[index];
+            } else {
+                clicker = new Clicker(imgURL, title);
+                clickerCache[index] = clicker;
+            }
+            let clickerEL = clicker.init();
+
+            clickerContainer.innerHTML = '';
+            clickerContainer.appendChild(clickerEL);
+        });
+
+        clickerItem.appendChild(btn);
+        fragment.appendChild(clickerItem);
     });
 
-    return function() {
-        countEl.textContent = count;
-    }
-})();
+    clickerList.appendChild(fragment);
+};
+
+initList();
