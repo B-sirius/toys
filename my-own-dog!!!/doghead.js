8b7f62ee86168aa6d301a227a4be4ce82bfe0355
b-sirius <padfoot_07@163.com>
2017-12-20 20:21:20 +0800

    you are a fake dog!!!

--- my-own-dog!!!/doghead.js
+++ my-own-dog!!!/doghead.js
@@ -35,28 +35,31 @@ function run(gen, ...args) {
 //===================== main part =========================
 // Snap element
 const dogSVG = Snap.select('#dogSVG');
+const dogGroup = Snap.select('#dogGroup');
 const bone = Snap.select('#bone');
 const eyeGroup = Snap.select('#eyeGroup');
 const eyeL = Snap.select('#eyeL');
 const eyeR = Snap.select('#eyeR');
 const eyeSpinL = Snap.select('#eyeSpinL');
 const eyeSpinR = Snap.select('#eyeSpinR');
+const earL = Snap.select('#earL');
+const earR = Snap.select('#earR');
 const browGroup = Snap.select('#browGroup');
 const browL = Snap.select('#browL');
 const browR = Snap.select('#browR');
 const snout = Snap.select('#snout');
 const nose = Snap.select('#nose');
+const noseShine = Snap.select('#noseShine');
 
 // 范围
-const eyeMaxY = 13;
+const eyeMaxY = 1;
 const browMaxY = 2;
 const browMaxRot = 0;
 const snoutMinY = 2;
-const snoutMaxY = 20;
+const snoutMaxY = 12;
 const noseMaxY = 12;
-
-// 唯一标识符
-const noseTransformAttr = Symbol('nose');
+const stageWidth = dogSVG.attr('width');
+const stageHeight = dogSVG.attr('height');
 
 // 禁用svg内所有图像的鼠标事件
 let disablePointEvents = (function () {
@@ -82,143 +85,120 @@ let disablePointEvents = (function () {
 })();
 
 // 初始化定位点
-Snap.set(nose, bone, eyeL, eyeR).attr({
-    'transform-origin': '50% 50%',
+TweenMax.set([nose.node, bone.node, eyeL.node, eyeR.node], {
+    transformOrigin: '50% 50%',
 });
-browL.attr({
-    'transform-origin': '0% 50%'
+TweenMax.set(browL.node, {
+    transformOrigin: '0% 50%'
 });
-browR.attr({
-    'transform-origin': '100% 150%'
+TweenMax.set(browR.node, {
+    transformOrigin: '100% 150%'
+});
+TweenMax.set([eyeSpinL.node, eyeSpinR.node], {
+    transformOrigin: '65% 50%'
 });
 
 // 眨眼
-let blink = (function () {
-    let close = function (eye) {
-        let promise = new Promise((resolve, reject) => {
-            Snap.animate(7, 0, function (val) {
-                eye.attr({
-                    ry: val
-                });
-            }, 50, undefined, resolve);
-        });
-
-        return promise;
-    }
-
-    let open = function (eye) {
-        let promise = new Promise((resolve, reject) => {
-            Snap.animate(0, 7, function (val) {
-                eye.attr({
-                    ry: val
-                });
-            }, 50, undefined, resolve);
-        });
-
-        return promise;
-    }
-
-    function* blinkEye(eye) {
-        yield close(eye);
-        yield open(eye);
-    };
-
-    setInterval(function () {
-        run(blinkEye, eyeL);
-        run(blinkEye, eyeR);
-    }, 5000);
-})();
+let blink = function () {
+    TweenMax.to([eyeL.node, eyeR.node], 0.1, {
+        attr: {
+            ry: 0
+        },
+        repeat: 1,
+        yoyo: true,
+        onComplete: blink,
+        delay: Math.random() * 10
+    });
+};
 
 // 嗅嗅
-let sniff = (function () {
-    nose[noseTransformAttr] = {
-        'scale': '',
-        'translate3d': '',
-    };
+let sniff = function () {
+    TweenMax.to(nose.node, 0.1, {
+        scaleX: 1.1,
+        repeat: 1,
+        yoyo: true,
+        onComplete: sniff,
+        delay: Math.random()
+    });
+}
 
-    function* main() {
-        yield (function () {
-            nose[noseTransformAttr]['scale'] = 'scale(1.1, 1)';
+// 鼻头高光，其实就是遮住一部分路径
+let noseShineTL = new TimelineMax({ paused: true });
 
-            let promise = new Promise((resolve, reject) => {
-                nose.animate({
-                    transform: `${nose[noseTransformAttr].scale} ${nose[noseTransformAttr].translate3d}`,
-                }, 100, undefined, resolve);
-            });
-
-            return promise;
-        })();
-
-        yield (function () {
-            nose[noseTransformAttr]['scale'] = 'scale(1, 1)';
-
-            let promise = new Promise((resolve, reject) => {
-                nose.animate({
-                    transform: `${nose[noseTransformAttr].scale} ${nose[noseTransformAttr].translate3d}`,
-                }, 100, undefined, resolve);
-            });
-
-            return promise;
-        })();
-    }
-
-    setInterval(function () {
-        run(main);
-    }, 700 * Math.random() + 1000);
-})();
+noseShineTL.fromTo(noseShine.node, 1, {
+    drawSVG: '0% 20%'
+}, {
+        drawSVG: '0% 50%',
+        ease: Linear.easeNone
+    }).to(noseShine.node, 1, {
+        drawSVG: '40% 60%',
+        ease: Linear.easeNone
+    }).to(noseShine.node, 1, {
+        drawSVG: '80% 100%',
+        ease: Linear.easeNone
+    });
 
 // 鼠标移动动态
 dogSVG.mousemove(function move(e) {
     // 鼠标相对狗头中心位置
     let rePos = {
-        x: ((e.offsetX - dogSVG.attr('width') / 2)),
-        y: ((e.offsetY - dogSVG.attr('height') / 2)),
+        x: ((e.offsetX - stageWidth / 2)),
+        y: ((e.offsetY - stageHeight / 2)),
     };
+    TweenMax.to(eyeGroup.node, 1, {
+        x: rePos.x / 20,
+        y: ((rePos.y / 12) > eyeMaxY) ? eyeMaxY : rePos.y / 12
+    });
 
-    // 眼睛组
-    eyeGroup.animate({
-        transform: `translate3d(${rePos.x / 20}, ${Math.abs(rePos.y / 12) > eyeMaxY ?
-            Math.sign(rePos.y / 12) * eyeMaxY :
-            (rePos.y / 12)})`,
-    }, 1000, mina.easein);
+    TweenMax.to(browL.node, 1, {
+        rotation: ((rePos.y / 25) > browMaxRot) ? browMaxRot : rePos.y / 25
+    });
+    TweenMax.to(browR.node, 1, {
+        rotation: -((rePos.y / 15) > browMaxRot) ? -browMaxRot : -rePos.y / 15
+    });
+    TweenMax.to(browGroup.node, 1, {
+        x: rePos.x / 40,
+        y: ((rePos.y / 25) > browMaxY) ? browMaxY : rePos.y / 25
+    });
 
-    // 左眉毛
-    browL.animate({
-        transform: `rotate(${-(rePos.y / 12) > browMaxRot ?
-            rePos.y / 24 :
-            browMaxRot
-            })`
-    }, 1000, mina.easein);
+    TweenMax.to(snout.node, 1, {
+        x: rePos.x / 30,
+        y: ((rePos.y / 60) < snoutMinY) ? snoutMinY : rePos.y / 60
+    });
+    TweenMax.to(nose.node, 1, {
+        x: rePos.x / 8,
+        y: ((rePos.y / 10) > noseMaxY) ? noseMaxY : rePos.y / 10
+    });
+    TweenMax.to([earL.node, earR.node], 1, {
+        x: -(rePos.x / 50),
+        y: -(rePos.y / 50)
+    });
+    TweenMax.to(dogGroup.node, 1, {
+        x: (rePos.x / 23),
+        y: (rePos.y / 23)
+    });
+    TweenMax.to(bone.node, 1, {
+        x: e.offsetX - (bone.getBBox().width / 2),
+        y: e.offsetY - (bone.getBBox().height / 2),
+        ease: Elastic.easeOut.config(0.7, 0.8)
+    })
 
-    // 右眉毛
-    browR.animate({
-        transform: `rotate(${-(rePos.y / 12) > browMaxRot ?
-            -rePos.y / 17 :
-            browMaxRot
-            })`
-    }, 1000, mina.easein);
-
-    // 眉毛组
-    browGroup.animate({
-        transform: `translate3d(${rePos.x / 40}, ${Math.abs(rePos.y / 25) > browMaxY ?
-            Math.sign(rePos.y / 40) * browMaxY :
-            (rePos.y / 25)})`
-    }, 1000, mina.easein);
-
-    // 大嘴盘子
-    snout.animate({
-        transform: `translate3d(${rePos.x / 30}, ${Math.abs(rePos.y / 40) > snoutMaxY ?
-            Math.sign(rePos.y / 40) * snoutMaxY :
-            (rePos.y / 40)})`
-    }, 1000, mina.easein);
-
-    // 鼻子
-    let noseX = rePos.x / 8,
-        noseY = Math.abs(rePos.y / 10) > noseMaxY ?
-            Math.sign(rePos.y / 40) * noseMaxY :
-            (rePos.y / 40);
-    nose[noseTransformAttr].translate3d = `translate3d(${noseX}, ${noseY})`;
-    nose.animate({
-        transform: `${nose[noseTransformAttr].scale} ${nose[noseTransformAttr].translate3d}`,
-    }, 1000, mina.easein);
+    TweenMax.set(noseShineTL, {
+        time: noseShineTL.duration() - (e.offsetX / stageWidth * noseShineTL.duration())
+    });
 });
+
+dogSVG.click(function (e) {
+    TweenMax.to([bone.node, eyeSpinL.node, eyeSpinR.node], 1, {
+        rotation: '+=720',
+        // 这里是将内部eye反向eyeSpin旋转，使眼睛只相对面部旋转而不改变其角度，这样在旋转时眨眼才不会鬼畜
+        onUpdate: function() {
+            TweenMax.set([eyeL.node, eyeR.node], {
+                rotation: -eyeSpinL.node._gsTransform.rotation
+            });
+        }
+    })
+})
+
+blink();
+sniff();
